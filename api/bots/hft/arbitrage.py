01b6f99a3c8b5766dd41976f33575b86ae588167
Adolpho <adolpho.igor@gmail.com>
2023-01-06 15:58:27 -0300

    Devel: Specializing bots into HFT and TR bots

--- api/bots/hft/arbitrage.py
+++ api/bots/hft/arbitrage.py
@@ -1,3 +1,7 @@
+import traceback
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from datetime import datetime
+
 from api.bots.tr.bot import Bot
 from api.jobs.internal_config_provider import InternalConfigProviders
 from api.logger import logger
@@ -36,6 +40,8 @@ class Arbitrage(Bot):
         self._lst_orders_0 = None
         self._lst_orders_1 = None
 
+        self._time_limit = datetime.strptime(self._algo.get("time_limit"), "%H:%M:%S")
+
     def _init_orders_instruments(self):
         if not self._dct_orders:
             dct_orders = {}
@@ -70,14 +76,20 @@ class Arbitrage(Bot):
             lst_spread
 
     def _execute(self):
+
+        # TODO: impor limite maximo de negociação, apos este momento não abre mais operações e encerra as que tiverem
+        # abertas.
+
         self._init_orders_instruments()
 
         # if it has been a previous negotiation still opened, must be keep it running in order to finish it.
         if not self._is_asset_state(["opened"]) and self._dct_orders and not (self._lst_orders_0 or self._lst_orders_1):
             return
 
+        b_in_session = datetime.now().time() <= self._time_limit.time()
+
         # entry point
-        if not self._lst_orders_0 and not self._lst_orders_1:
+        if b_in_session and not self._lst_orders_0 and not self._lst_orders_1:
             lst_signal, lst_spread = self._get_signal()
             # TODO: Retirar
             lst_signal = [True, False]
@@ -133,53 +145,72 @@ class Arbitrage(Bot):
         '''
         # exit point
         if self._lst_orders_0 and self._lst_orders_1:
-
             lst_signal, lst_spread = self._get_signal()
             # TODO: Retirar
             lst_signal = [False, False]
             if not any(lst_signal) or not self._is_asset_state(["opened"]):
-                lst_proc = [self._lst_orders_0, self._lst_orders_1]
-                for proc in lst_proc:
+                with ThreadPoolExecutor(max_workers=1) as executor:
+                    lst_thr = [executor.submit(self._proc_orders_list, lst[1], lst[0])
+                               for lst in [(self._lst_orders_0, lst_spread[0]), (self._lst_orders_1, lst_spread[1])]]
 
-                    lst_copy = proc[::-1]
-                    for ordr in lst_copy:
-                        symbol = ordr.get("symbol")
-                        arm = self._tpl_arm_0 if ordr.get("symbol") == self._tpl_arm_0[3] else self._tpl_arm_1
+                    for thr in as_completed(lst_thr):
+                        excpt = thr.exception()
+                        if excpt:
+                            logger.error(excpt)
 
-                        status = ordr.get("status")
+                        rslt = thr.result()
+                        if rslt:
+                            logger.info(rslt)
 
-                        if status == "Rejected":
-                            logger.error(f"Error in Arbitrage. Symbol: {symbol}. Msg: {ordr.get('text_message', '')}.")
+            if not self._lst_orders_0 and not self._lst_orders_1:
+                self._print_positions()
 
-                        if status == "New":
-                            self._profit_dll.send_cancel_order(conta=arm[0], broker=arm[1], senha=arm[2],
-                                                               cl_ord_id=ordr.get("cl_ord_id"))
+    def _proc_orders_list(self, lst_spread: list, lst_orders: list):
+        lst_copy = lst_orders[::-1]
+        for ordr in lst_copy:
+            symbol = ordr.get("symbol")
+            arm = self._tpl_arm_0 if ordr.get("symbol") == self._tpl_arm_0[3] else self._tpl_arm_1
 
-                        if status == "Filled":
-                            if ordr.get("side") == "S":
-                                self._profit_dll.send_buy_order(conta=arm[0], broker=arm[1], senha=arm[2],
-                                                                ativo=arm[3], bolsa=arm[4], preco=lst_spread[1][1],
-                                                                qtd=ordr.get("qtd"))
-                            else:
-                                self._profit_dll.send_sell_order(conta=arm[0], broker=arm[1], senha=arm[2],
-                                                                 ativo=arm[3], bolsa=arm[4], preco=lst_spread[0][1],
-                                                                 qtd=ordr.get("qtd"))
-                        if status == "PartiallyFilled":
-                            self._profit_dll.send_cancel_order(conta=arm[0], broker=arm[1], senha=arm[2],
-                                                               cl_ord_id=ordr.get("cl_ord_id"))
+            status = ordr.get("status")
 
-                            if ordr.get("side") == "S":
-                                self._profit_dll.send_buy_order(conta=arm[0], broker=arm[1], senha=arm[2],
-                                                                ativo=arm[3], bolsa=arm[4], preco=lst_spread[1][1],
-                                                                qtd=ordr.get("qtd"))
-                            else:
-                                self._profit_dll.send_sell_order(conta=arm[0], broker=arm[1], senha=arm[2],
-                                                                 ativo=arm[3], bolsa=arm[4], preco=lst_spread[0][1],
-                                                                 qtd=ordr.get("qtd"))
+            text_message = ordr.get('text_message', '')
+            if status == "Rejected" and text_message:
+                logger.error(f"Error in Arbitrage. Symbol: {symbol}. Msg: {text_message}.")
 
-                            lst_rem = [rem for rem in proc if rem.get("profit_id") == ordr.get("profit_id")]
-                            for rem in lst_rem:
-                                proc.remove(rem)
+            try:
+                if status == "New":
+                    self._profit_dll.send_cancel_order(conta=arm[0], broker=arm[1], senha=arm[2],
+                                                       cl_ord_id=ordr.get("cl_ord_id"))
 
-                        proc.remove(ordr)
-                        lst_copy = proc[::-1]
+                # Compra = 1, Venda = 2
+                if status == "Filled":
+                    if ordr.get("side") == 2:
+                        self._profit_dll.send_buy_order(conta=arm[0], broker=arm[1], senha=arm[2],
+                                                        ativo=arm[3], bolsa=arm[4], preco=lst_spread[1][1],
+                                                        qtd=ordr.get("qtd"))
+                    else:
+                        self._profit_dll.send_sell_order(conta=arm[0], broker=arm[1], senha=arm[2],
+                                                         ativo=arm[3], bolsa=arm[4], preco=lst_spread[0][1],
+                                                         qtd=ordr.get("qtd"))
+                if status == "PartiallyFilled":
+                    self._profit_dll.send_cancel_order(conta=arm[0], broker=arm[1], senha=arm[2],
+                                                       cl_ord_id=ordr.get("cl_ord_id"))
+
+                    if ordr.get("side") == 2:
+                        self._profit_dll.send_buy_order(conta=arm[0], broker=arm[1], senha=arm[2],
+                                                        ativo=arm[3], bolsa=arm[4], preco=lst_spread[1][1],
+                                                        qtd=ordr.get("qtd"))
+                    else:
+                        self._profit_dll.send_sell_order(conta=arm[0], broker=arm[1], senha=arm[2],
+                                                         ativo=arm[3], bolsa=arm[4], preco=lst_spread[0][1],
+                                                         qtd=ordr.get("qtd"))
+
+                    lst_rem = [rem for rem in lst_orders if rem.get("profit_id") == ordr.get("profit_id")]
+                    for rem in lst_rem:
+                        lst_orders.remove(rem)
+
+            except Exception as e:
+                logger.error(traceback.format_exc())
+
+            lst_orders.remove(ordr)
+            lst_copy = lst_orders[::-1]
--- api/bots/tr/bot.py
+++ api/bots/tr/bot.py
@@ -57,6 +57,15 @@ class Bot(Thread):
         self.__unsubscribe()
         logger.info(f"The algo name: {self.name} was finalized.")
 
+    def _print_positions(self):
+        dct_ret = {}
+        for sbl in self._algo.get("threads"):
+            dct_brkr = sbl.get("broker")
+            dct_ret.update(self._profit_dll.get_position(conta=dct_brkr.get("account"), broker=dct_brkr.get("id"),
+                                                         ativo=sbl.get("symbol"), bolsa=dct_brkr.get("stock_market")))
+
+        logger.info(f"Algo {self.name} Actual position: {dct_ret}")
+
     def __test_qtd_assets(self):
         qtd_ast = len(set(self._lst_sbl))
 
