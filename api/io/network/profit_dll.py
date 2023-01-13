b8e662d0cecce39ca70ad42549082e721389fa7a
Adolpho <adolpho.igor@gmail.com>
2023-01-13 18:50:03 -0300

    Devel: Lots of preformance ajustments and bugfixes

--- api/io/network/profit_dll.py
+++ api/io/network/profit_dll.py
@@ -856,7 +856,8 @@ class ProfitDLL:
         # print(f"symbol: {asset_id.ticker}, side: {side}, value: {lst_book[side][::-1][0][:2]}, action: {action}, "
         #       f"price: {price}, qtd: {qtd}, count: {count}")
 
-        lst_spread_rt[side] = lst_book[side][::-1][0][:2][::-1]
+        if lst_book[side][::-1][0]:
+            lst_spread_rt[side] = lst_book[side][::-1][0][:2][::-1]
 
     def offer_book_callback(self, asset_id, action, position, side, qtd, agent, offer_id, price, has_price, has_qtd,
                             has_date, has_offer_id, has_agent, date, array_sell, array_buy):
