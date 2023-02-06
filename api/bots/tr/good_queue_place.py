cb5259f02e6017371509462b8f376e7bc1e8f5a3
Adolpho <adolpho.igor@gmail.com>
2023-02-06 14:50:22 -0300

    DEVEL: TRBOTS architecture development in progress.

--- api/bots/tr/good_queue_place.py
+++ api/bots/tr/good_queue_place.py
@@ -1,8 +1,8 @@
-from api.bots.bot import Bot
+from api.bots.tr.tr_bot import TRBot
 from api.jobs.internal_config_provider import InternalConfigProviders
 
 
-class GoodQueuePlace(Bot):
+class GoodQueuePlace(TRBot):
 
     def __init__(self, name, daemon, algo: dict, config_prov: InternalConfigProviders):
         super().__init__(name, daemon, algo, Bot.ARM_ONE, config_prov)
@@ -25,71 +25,69 @@ class GoodQueuePlace(Bot):
 
         return [find_level(self._lst_lp[0]), find_level(self._lst_lp[1])]
 
-    def _execute(self):
-        pass
-        # entry point.
-        """
-        lst_lpc = [[7000, 1575.5], [280, 1575.0], [200, 1574.5], [50, 1574.0], [125, 1573.5], [160, 1573.0]]
-        lst_lpv = [[12000, 1575.5], [100, 1576.0], [300, 1576.5], [200, 1577.0], [250, 1577.5], [125, 1578.0]]
-        lst_lp = [lst_lpc, lst_lpv]
-        
-        def get_smaller_level(lst_lp: list):
-            last_level_val = -1
-            last_level_price = -1
-            last_level_index = -1
-            for i in range(len(lst_lp) - 1):
-                if last_level_val == -1 or lst_lp[i][0] < last_level_val:
-                    last_level_val = lst_lp[i][0]
-                    last_level_price = lst_lp[i][1]
-                    last_level_index = i
-        
-            return [last_level_val, last_level_price, last_level_index]
-        
-        lst_result = [get_smaller_level(lst_lpc), get_smaller_level(lst_lpv)]
-        print(lst_result)
-        
-        
-        qtd = lst_lp[0][0][0] - lst_lp[1][0][0] + lst_lp[0][1][0] - lst_lp[1][1][0]
-        
-        lst_niv_menor_liq = None
-        side = "B" if qtd > 0 else "S"
-        qtd = abs(qtd)
-        nivel_p_dentro = 2
-        while True:
-            if qtd <= 0:
-                break
-        
-            try:
-                qtd_nivel = lst_lp[1][nivel_p_dentro][0] if side == "B" else lst_lp[0][nivel_p_dentro][0]
-                qtd -= qtd_nivel
-        
-            except Exception:
-                break
-        
-            if nivel_p_dentro == 2:
-                lst_niv_menor_liq = [nivel_p_dentro, qtd_nivel]
-        
-            elif lst_niv_menor_liq[1] > qtd_nivel:
-                lst_niv_menor_liq[0] = nivel_p_dentro
-                lst_niv_menor_liq[1] = qtd_nivel
-        
-            nivel_p_dentro += 1
-        
-        print(f"side: {side} saldo final: {qtd} niveis p/ dentro: {nivel_p_dentro}")
-
-        """
-
-    def _thr_spread(self):
+    def _thr_spread(self, name):
         pass
 
-    def _thr_book(self):
+    def _thr_book(self, name):
+        self._get_smaller_level()
+
+    def _thr_order_flow(self, name):
         pass
 
-    def _thr_order_flow(self):
+    def _thr_scheduled_news(self, name):
         pass
 
-    def _thr_scheduled_news(self):
+    def _thr_breaking_news(self, name):
         pass
 
-    def _thr_breaking_news(self):
-        pass
+    # entry point.
+    """
+    lst_lpc = [[7000, 1575.5], [280, 1575.0], [200, 1574.5], [50, 1574.0], [125, 1573.5], [160, 1573.0]]
+    lst_lpv = [[12000, 1575.5], [100, 1576.0], [300, 1576.5], [200, 1577.0], [250, 1577.5], [125, 1578.0]]
+    lst_lp = [lst_lpc, lst_lpv]
+
+    def get_smaller_level(lst_lp: list):
+        last_level_val = -1
+        last_level_price = -1
+        last_level_index = -1
+        for i in range(len(lst_lp) - 1):
+            if last_level_val == -1 or lst_lp[i][0] < last_level_val:
+                last_level_val = lst_lp[i][0]
+                last_level_price = lst_lp[i][1]
+                last_level_index = i
+
+        return [last_level_val, last_level_price, last_level_index]
+
+    lst_result = [get_smaller_level(lst_lpc), get_smaller_level(lst_lpv)]
+    print(lst_result)
+
+
+    qtd = lst_lp[0][0][0] - lst_lp[1][0][0] + lst_lp[0][1][0] - lst_lp[1][1][0]
+
+    lst_niv_menor_liq = None
+    side = "B" if qtd > 0 else "S"
+    qtd = abs(qtd)
+    nivel_p_dentro = 2
+    while True:
+        if qtd <= 0:
+            break
+
+        try:
+            qtd_nivel = lst_lp[1][nivel_p_dentro][0] if side == "B" else lst_lp[0][nivel_p_dentro][0]
+            qtd -= qtd_nivel
+
+        except Exception:
+            break
+
+        if nivel_p_dentro == 2:
+            lst_niv_menor_liq = [nivel_p_dentro, qtd_nivel]
+
+        elif lst_niv_menor_liq[1] > qtd_nivel:
+            lst_niv_menor_liq[0] = nivel_p_dentro
+            lst_niv_menor_liq[1] = qtd_nivel
+
+        nivel_p_dentro += 1
+
+    print(f"side: {side} saldo final: {qtd} niveis p/ dentro: {nivel_p_dentro}")
+
+    """
--- api/bots/tr/tr_bot.py
+++ api/bots/tr/tr_bot.py
@@ -1,3 +1,5 @@
+import threading
+
 from api.bots.bot import Bot
 from api.bots.tr.position import PositionMgr
 from api.jobs.internal_config_provider import InternalConfigProviders
@@ -5,10 +7,11 @@ from api.jobs.internal_config_provider import InternalConfigProviders
 
 class TRBot(Bot):
 
-    _lst_orders_sent = []
-
     def __init__(self, name, daemon, algo: dict, qtd_exp: int, config_prov: InternalConfigProviders):
         super().__init__(name, daemon, algo, qtd_exp, config_prov)
+
+        self._lst_threads = []
+        self._dct_threads = {}
 
     def _execute(self):
         """
@@ -16,23 +19,32 @@ class TRBot(Bot):
             they need. Subclass has no need to implement this method.
         :return:
         """
-        pass
+        def create_thread(thr_name, thr_target):
+            thr = threading.Thread(target=thr_target, name=thr_name, daemon=True, args=(thr_name,))
+            self._lst_threads.append(thr)
+            thr.start()
 
-    def _thr_spread(self):
+        create_thread("0_MANGR_SPREAD", self._thr_spread)
+        create_thread("1_MANGR_BOOK", self._thr_book)
+        create_thread("2_MANGR_ORD_FLOW", self._thr_order_flow)
+        create_thread("3_MANGR_NEWS", self._thr_scheduled_news)
+        create_thread("4_MANGR_BRK_NEWS", self._thr_breaking_news)
+
+    def _thr_spread(self, name):
         """
             Manages the operations on the top of the book (only), so, gets the highest priority of any other threads.
         :return:
         """
         pass
 
-    def _thr_book(self):
+    def _thr_book(self, name):
         """
             Manages to find the best queue place at the closer prices as possible.
         :return:
         """
         pass
 
-    def _thr_order_flow(self):
+    def _thr_order_flow(self, name):
         """
             Manages to alert the main thread to cancel and or exit current operations if an unusual strong income flow
             in the correlated assets were detected;
@@ -40,7 +52,7 @@ class TRBot(Bot):
         """
         pass
 
-    def _thr_scheduled_news(self):
+    def _thr_scheduled_news(self, name):
         """
             Manages to alert the main thread to cancel and or exit current operations when a high volatility scheduled
             report is about to be announced.
@@ -48,7 +60,7 @@ class TRBot(Bot):
         """
         pass
 
-    def _thr_breaking_news(self):
+    def _thr_breaking_news(self, name):
         """
             Manages to alert the main thread to cancel and or exit current operations when important breakiung news
             is announced.
